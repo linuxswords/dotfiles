574a432a9ab1faf78e49e49bd69e07d65c56060f
Martin Knoller Stocker <martin.knoller@brackalltron.ch>
2026-05-07 09:41:03 +0200

    nvim: leap plugin update

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,10 +1,3 @@
 return {
-  url = "https://codeberg.org/andyg/leap.nvim",
-  keys = false,
-  config = function(_, opts)
-    local leap = require("leap")
-    -- add your own keymaps here
-    vim.keymap.set({ "n", "x", "o" }, "<leader>j", "<Plug>(leap)")
-    vim.keymap.set("n", "<leader>J", "<Plug>(leap-from-window)")
-  end,
+  url = "https://codeberg.org/andyg/leap.nvim.git",
 }
